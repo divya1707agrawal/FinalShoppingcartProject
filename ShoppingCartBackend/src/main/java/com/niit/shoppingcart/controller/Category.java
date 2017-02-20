f8a6913a72f645d61c692df3c4e7a0bb2c4e3c8d
divya1707agrawal <divya1707agrawal@gmail.com>
2017-02-20 15:09:37 +0530

    Add files via upload

---
+++ ShoppingCartBackend/src/main/java/com/niit/shoppingcart/controller/Category.java
@@ -0,0 +1,73 @@
+package com.niit.shoppingcart.controller;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.annotation.*;
+
+@Entity
+@Table(name="category")
+@Component
+public class Category{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String Id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "description")
+	private String description;
+	
+	@JsonManagedReference
+	@OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+	private Set<Product> product;
+
+	public Set<Product> getProducts() {
+		return product;
+    }
+
+	public void setProducts(Set<Product> product) {
+		this.product = product;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "{" + Id + "," + name + "," + description + "}";
+	}
+}
